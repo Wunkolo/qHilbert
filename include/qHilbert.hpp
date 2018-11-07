e0a604cebd7deffb45c69369af8aac521c47905b
Wunkolo <Wunkolo@gmail.com>
2018-11-07 12:33:50 -0800

    Refactor serial implementation

--- include/qHilbert.hpp
+++ include/qHilbert.hpp
@@ -4,13 +4,13 @@
 #include <glm/glm.hpp>
 
 void qHilbertSerial(
-	std::size_t Width, // Must be a power of 2
+	std::size_t Size, // Must be a power of 2
 	std::uint32_t Distance,
 	glm::u32vec2& Position
 );
 
 void qHilbert(
-	std::size_t Width, // Must be power of 2
+	std::size_t Size, // Must be power of 2
 	const std::uint32_t Distances[],
 	glm::u32vec2 Positions[],
 	std::size_t Count
