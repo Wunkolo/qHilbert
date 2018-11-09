31b11ee40c572e34c2bf3e411efe2ab0a9dd6428
Wunkolo <Wunkolo@gmail.com>
2018-11-08 23:33:16 -0800

    Remove qHilbertSerial api

--- include/qHilbert.hpp
+++ include/qHilbert.hpp
@@ -3,12 +3,6 @@
 #include <cstddef>
 #include <glm/glm.hpp>
 
-void qHilbertSerial(
-	std::size_t Size, // Must be a power of 2
-	std::uint32_t Distance,
-	glm::u32vec2& Position
-);
-
 void qHilbert(
 	std::size_t Size, // Must be power of 2
 	const std::uint32_t Distances[],
